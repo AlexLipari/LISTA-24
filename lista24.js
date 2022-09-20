5279664c2bff10ac2945c0f291468dad00dfb638
Alexandre Lipari <santuslipari@gmail.com>
2022-09-20 12:14:08 -0300

    Utilizando forEach para procurar no array de id reserva de um hotel e apresentar lista, complemento calculadora aula objetos.

---
+++ lista24.js
@@ -0,0 +1,73 @@
+let idHotelreserva = [1,3,4,1,5]
+let idHotel = [1,2,3,4,5]
+let nomesHotel = ["Ibis","Hilton","Copacabana Palace","Emiliano","Ocean Palace"]
+let categoriasHotel = ["****","*****","*****","****","***"]
+let enderecosHotel = ["SP","RJ","RJ","SC","RN"]
+let telefonesHotel = ["(11)","(21)","(21)","(47)","(84)"]
+let idReserva = [6,7,8,9,10]
+let nomesReserva = ["Sara","Bianca","Cris","Fabiana","Ligia"]
+let entradasReserva = [1,2,3,4,5]
+let saidasReserva = [5,6,7,8,9,]
+ // cadastrar()
+let idUsuario = parseInt(prompt("Digite o id do hotel que contem a reserva:")) 
+procurarIdHotel(idUsuario)
+// function cadastrar (){
+//     idHotel.push(prompt("Digite o id do Hotel:"))
+//     nomesHotel.push(prompt("Digite o nome do hotel:"))
+//     categoriasHotel.push(prompt("Digite a categoria do hotel:"))
+//     enderecosHotel.push(prompt("Digite o endereço do hotel:"))
+//     telefonesHotel.push(prompt("Digite o telefone do hotel:"))
+
+//     console.log(idHotel,"-",nomesHotel,"-",categoriasHotel,"-",enderecosHotel,"-",telefonesHotel)
+
+//     idReserva.push(prompt("Digite o id de reserva:"))
+//     nomesReserva.push(prompt("Digite o nome da Reserva:"))
+//     entradasReserva.push(prompt("Digite a entrada da reserva:"))
+//     saidasReserva.push(prompt("Digite a saida da reserva:"))
+
+//     console.log(idReserva,"-",nomesReserva,"-",entradasReserva,"-",saidasReserva)
+//}
+
+function procurarIdHotel(parametroIdHotel){
+      let hotelIdReservaAux=[]
+    idHotelreserva.forEach((idUsuario,index) =>{
+        if(idUsuario == parametroIdHotel){
+            console.log("Id da reserva:",parametroIdHotel)
+            hotelIdReservaAux[hotelIdReservaAux.length]=console.log("Id do Hotel:", idHotel[index])
+            hotelIdReservaAux[hotelIdReservaAux.length]=console.log("Nome do Hotel:", nomesHotel[index])
+            hotelIdReservaAux[hotelIdReservaAux.length]=console.log("Ospede:", nomesReserva[index])
+            hotelIdReservaAux[hotelIdReservaAux.length]=console.log("dia de entrada:", entradasReserva[index])
+            hotelIdReservaAux[hotelIdReservaAux.length]=console.log("dia de saída:", saidasReserva[index])
+        }
+    })
+     hotelIdReservaAux.forEach(idUsuario => console.log(idUsuario))
+}
+
+
+
+
+// class Calculadora {
+//     NumeroUm
+//     NumeroDois
+    
+//     somar(){
+//         console.log(this.NumeroUm + this.NumeroDois)
+//     }
+//     subtrair(){
+//         console.log(this.NumeroUm  - this.NumeroDois)
+//     }
+//     multiplicar(){
+//         console.log(this.NumeroUm  * this. NumeroDois)
+//     }
+//     dividir(){
+//         console.log(this.NumeroUm  / this.NumeroDois)
+//     }
+// }
+// let calculadora = new Calculadora()
+// calculadora.NumeroUm = 10
+// calculadora.NumeroDois = 20
+
+// calculadora.somar()
+// calculadora.subtrair()
+// calculadora.multiplicar()
+// calculadora.dividir()
